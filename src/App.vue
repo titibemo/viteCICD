c47b8c288d2bd3621f5ece1288558c539b8f6965
Titi <levendeurlillois@hotmail.fr>
2024-11-19 10:24:58 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  <h1>YEAH J'ai réussi - Thierry -</h1>
+  <h1>YEAH J'ai réussi - Thierry -
+    Tu as un entretien à 13h30 sur zoom avec Boris !!!
+  </h1>
 </template>
 
 <style scoped>
